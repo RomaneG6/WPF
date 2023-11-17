859e066bfe89dbe77b3ad70a646b4eb2b96ce53f
Grootaers Romane <210469@CCM-Asty-Moulin.local>
2023-11-17 14:37:39 +0100

    interro

---
+++ UAA14_I3_Grootaers/UAA14_I3_Grootaers/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UAA14_I3_Grootaers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Calculer.Click += new RoutedEventHandler(Calculer_Click);
+            Nombre1.KeyDown += new KeyEventArgs(Nombre_key);
+            Nombre2.KeyDown += new KeyEventArgs(Nombre_key);
+        }
+
+        private void Calculer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nombre_key(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
